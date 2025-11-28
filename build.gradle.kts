b31c3e3dc6cb28c6d92a0938d1d2dedfe7f40edc
euluan1912 <euluan1912@gmail.com>
2025-11-28 16:22:31 -0300

    fix: correct typo in recloudstream gradle classpath (master-SNAPSHOT)

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,7 +12,7 @@ buildscript {
 
     dependencies {
         classpath("com.android.tools.build:gradle:8.7.3")
-        classpath("com.github.recloudstream:gradle:master-SNnAPSHOT")
+        classpath("com.github.recloudstream:gradle:master-SNNAPSHOT")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.1.0")
     }
 }
