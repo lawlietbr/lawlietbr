e1c0961be2761e2d477be471176c9cb53cda2341
euluan1912 <euluan1912@gmail.com>
2025-12-01 20:59:34 -0300

    Atualizar o UltraCinePlugin.kt

--- UltraCine/src/main/kotlin/com/UltraCine/UltraCinePlugin.kt
+++ UltraCine/src/main/kotlin/com/UltraCine/UltraCinePlugin.kt
@@ -1,13 +1,14 @@
 package com.UltraCine
 
-import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
-import com.lagradost.cloudstream3.plugins.BasePlugin
+import com.lagradost.cloudstream3.extractors.VidStack
 
-@CloudstreamPlugin
-class UltraCinePlugin : BasePlugin() {
-    override fun load() {
-        registerMainAPI(UltraCine())
-        registerExtractorAPI(EmbedPlayUpnsPro())
-        registerExtractorAPI(EmbedPlayUpnOne())
-    }
+// Extratores para EmbedPlay (UpnsPro e UpnOne)
+class EmbedPlayUpnsPro : VidStack() {
+    override var name = "EmbedPlay UpnsPro"
+    override var mainUrl = "https://embedplay.upns.pro"
+}
+
+class EmbedPlayUpnOne : VidStack() {
+    override var name = "EmbedPlay UpnOne"
+    override var mainUrl = "https://embedplay.upn.one"
 }
