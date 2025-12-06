139abce2425362a88a1e3dfaaedd72cf3529c082
L <euluan1912@gmail.com>
2025-12-06 10:13:52 -0300

    Atualizar o UltraCineProvider.kt

--- UltraCine/src/main/kotlin/com/UltraCine/UltraCineProvider.kt
+++ UltraCine/src/main/kotlin/com/UltraCine/UltraCineProvider.kt
@@ -3,27 +3,19 @@ package com.UltraCine
 import com.lagradost.cloudstream3.plugins.BasePlugin
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 
-// IMPORTS CORRIGIDAS:
-// Importa o extrator do subpacote 'extractors' (PlayEmbedApiSite)
+// 1. IMPORTA DO SUBPACOTE 'extractors'
 import com.UltraCine.extractors.PlayEmbedApiSite
 
-// Importa os novos extratores que estão no PACOTE PRINCIPAL (com.UltraCine)
-import com.UltraCine.EmbedPlayUpnsInk
-import com.UltraCine.EmbedPlayUpnsPro
-import com.UltraCine.EmbedPlayUpnOne
-
+// 2. IMPORTA DO PACOTE PRINCIPAL 'com.UltraCine' (onde estão os outros extratores)
+import com.UltraCine.EmbedPlayUpnsInk // Este é o que lida com .ink
 
 @CloudstreamPlugin
 class UltraCineProvider : BasePlugin() {
     override fun load() {
         registerMainAPI(UltraCine()) 
         
-        // REGISTRA OS EXTRATORES CONFIRMADOS PELO DOMÍNIO ULTRACINE:
-        registerExtractorAPI(PlayEmbedApiSite()) // Para 'playembedapi.site'
-        registerExtractorAPI(EmbedPlayUpnsInk()) // Para 'embedplay.upns.ink'
-        
-        // MANTENHA ESTES CASO SEJAM NECESSÁRIOS:
-        // registerExtractorAPI(EmbedPlayUpnsPro())
-        // registerExtractorAPI(EmbedPlayUpnOne())
+        // REGISTROS:
+        registerExtractorAPI(PlayEmbedApiSite()) 
+        registerExtractorAPI(EmbedPlayUpnsInk()) // Deve resolver o link .ink
     }
 }
