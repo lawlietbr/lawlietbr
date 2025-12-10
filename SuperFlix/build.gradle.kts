ece23906059041d2035328f1ae9b0847dae2356e
L <euluan1912@gmail.com>
2025-12-10 13:14:21 -0300

    Update author in build.gradle.kts
    Signed-off-by: L <euluan1912@gmail.com>

--- SuperFlix/build.gradle.kts
+++ SuperFlix/build.gradle.kts
@@ -3,7 +3,7 @@ version = 1
 cloudstream {
     description = "SuperFlix - Filmes e Séries em Português"
     language = "pt-br"
-    authors = listOf("euluanzz")
+    authors = listOf("lietbr")
     status = 1
     tvTypes = listOf("Movie", "TvSeries")
     iconUrl = "https://superflix21.lol/assets/logo.png"
